e46762797c496f2a277f56703886bfe9d6627cee
Philip <philip.alexander.hassialis@gmail.com>
2023-01-27 01:33:38 +0200

    Initial commit

---
+++ src/main/java/com/in28minutes/learnspringframework/game/MarioGame.java
@@ -0,0 +1,19 @@
+package com.in28minutes.learnspringframework.game;
+
+public class MarioGame {
+    public void up() {
+        System.out.println("Mario going up");
+    }
+
+    public void down() {
+        System.out.println("Mario going down");
+    }
+
+    public void left() {
+        System.out.println("Mario going left");
+    }
+
+    public void right() {
+        System.out.println("Mario going right");
+    }
+}
---
+++ src/main/java/com/in28minutes/learnspringframework/game/SuperContraGame.java
@@ -0,0 +1,19 @@
+package com.in28minutes.learnspringframework.game;
+
+public class SuperContraGame {
+    public void up() {
+        System.out.println("Contra going up");
+    }
+
+    public void down() {
+        System.out.println("Contra going down");
+    }
+
+    public void left() {
+        System.out.println("Contra going left");
+    }
+
+    public void right() {
+        System.out.println("Contra going right");
+    }
+}
